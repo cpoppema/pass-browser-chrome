028a16a86ec594e14a5571aff6fcc48002d80c95
cpoppema <cpoppema@gmail.com>
2016-01-18 20:21:07 +0100

    Send public key as authentication when retrieving secrets

--- extensions/chrome/code/js/background.js
+++ extensions/chrome/code/js/background.js
@@ -82,10 +82,18 @@
           }
         }
 
-        var client = new XMLHttpRequest();
-        client.onload = handler;
-        client.open('GET', secretsUri);
-        client.send();
+        chrome.storage.local.get('public_key', function(items) {
+          // provide public key id as authentication
+          var publicKey = openpgp.key.readArmored(items.public_key).keys[0];
+          var longKeyId = publicKey.primaryKey.getKeyId().toHex().toUpperCase();
+          var shortKeyId = longKeyId.substr(-8);
+
+          var client = new XMLHttpRequest();
+          client.onload = handler;
+          client.open('POST', secretsUri);
+          client.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+          client.send(JSON.stringify({longKeyId: longKeyId, shortKeyId: shortKeyId}));
+        });
       });
     },
 
